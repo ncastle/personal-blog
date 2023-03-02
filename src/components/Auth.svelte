eab279da0abcb0a8eea28868ac9b3f255dd7ba4a
Nick Castle <castle5896@gmail.com>
2023-03-01 20:48:05 -0500

    use email/password login for authentication

--- src/components/Auth.svelte
+++ src/components/Auth.svelte
@@ -3,14 +3,33 @@
 
 	let loading = false;
 	let email: string;
+	let password: string;
+	let formMode: 'login' | 'signup' = 'login';
+	$: modeString = formMode === 'login' ? 'Log In' : 'Sign Up';
 
 	// login function with One Time Password / Email 'Magic' Link
 	const handleLogin = async () => {
 		try {
 			loading = true;
-			const { error } = await supabase.auth.signInWithOtp({ email });
+			const { error } = await supabase.auth.signInWithPassword({ email, password });
 			if (error) throw error;
-			alert('Check your email for the login link!');
+			alert('Successfully logged in!');
+		} catch (error) {
+			if (error instanceof Error) {
+				console.log({ error });
+				alert(error.message);
+			}
+		} finally {
+			loading = false;
+		}
+	};
+
+	const handleSignUp = async () => {
+		try {
+			loading = true;
+			const { data, error } = await supabase.auth.signUp({ email, password });
+			console.log({ data });
+			if (error) throw error;
 		} catch (error) {
 			if (error instanceof Error) {
 				alert(error.message);
@@ -19,18 +38,55 @@
 			loading = false;
 		}
 	};
+
+	const handleSubmit = () => {
+		if (formMode === 'signup') {
+			handleSignUp();
+		} else {
+			handleLogin();
+		}
+	};
 </script>
 
-<form on:submit|preventDefault={handleLogin}>
-	<h1>Login using magic link</h1>
-	<input type="email" bind:value={email} />
-	<input type="submit" value={loading ? 'Loading' : 'Log in with magic link'} disabled={loading} />
+<form on:submit|preventDefault={handleSubmit}>
+	<div>
+		<button type="button" on:click={() => (formMode = 'login')}>Login</button>
+		<button type="button" on:click={() => (formMode = 'signup')}>Sign Up</button>
+	</div>
+
+	<h1>{modeString} using email & password</h1>
+	<input type="email" bind:value={email} required />
+	<input type="password" bind:value={password} required />
+	<input type="submit" value={loading ? 'Loading' : modeString} disabled={loading} />
 </form>
 
 <style>
 	form {
+		margin: 2rem auto 0 auto;
+		width: 60vw;
+		border: 1px solid grey;
+		border-radius: 1rem;
+		box-shadow: 2px 2px 2px lightgrey, 4px 4px 4px grey;
 		display: flex;
 		flex-direction: column;
-		width: 30vw;
+		gap: 1rem;
+		padding: 1rem;
+	}
+
+	form > *:not(h1) {
+		font-size: 1rem;
+	}
+	input {
+		padding: 1rem;
+		border-radius: 0.5rem;
+	}
+	input[type='submit'] {
+		cursor: pointer;
+	}
+
+	button {
+		padding: 0.5rem;
+		border-radius: 0.5rem;
+		cursor: pointer;
 	}
 </style>
--- src/routes/account/+page.svelte
+++ src/routes/account/+page.svelte
@@ -1,5 +1,45 @@
-<script>
+<script lang="ts">
 	import Auth from '$components/Auth.svelte';
+	import { supabase } from '$lib/supabase';
+	import type { AuthSession } from '@supabase/supabase-js';
+	import { page } from '$app/stores';
+
+	let loading = false;
+
+	$: session = $page.data.session as AuthSession;
+	$: user = session?.user;
+
+	const signOut = async () => {
+		try {
+			loading = true;
+			let { error } = await supabase.auth.signOut();
+			if (error) throw error;
+		} catch (error) {
+			if (error instanceof Error) {
+				alert(error.message);
+			}
+		} finally {
+			loading = false;
+		}
+	};
 </script>
 
-<Auth />
+{#if user}
+	<h1>Welcome back, {user.email}</h1>
+	<button type="button" on:click={signOut} hidden={!session}>Sign out</button>
+{:else}
+	<Auth />
+{/if}
+
+<style>
+	h1 {
+		text-align: center;
+		display: inline;
+	}
+
+	button {
+		cursor: pointer;
+		padding: 0.5rem;
+		border-radius: 0.5rem;
+	}
+</style>
